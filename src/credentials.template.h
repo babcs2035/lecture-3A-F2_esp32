8168d3b511227046b336a5121c0573e7ea78a7e8
Kosei Takahashi <babcs2035@gmail.com>
2025-01-07 18:33:45 +0900

    [refactor] codes

---
+++ src/credentials.template.h
@@ -0,0 +1,11 @@
+// WiFi
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+
+// Firebase
+#define FIREBASE_API_KEY ""
+#define FIREBASE_DATABASE_URL ""
+
+// Device
+#define DEVICE_ID ""
+#define AD_PIN
--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,20 @@
-
 #include <Arduino.h>
-#if defined(ESP32)
 #include <WiFi.h>
-#elif defined(ESP8266)
-#include <ESP8266WiFi.h>
-#endif
 #include <Firebase_ESP_Client.h>
+#include "addons/TokenHelper.h"
+#include "addons/RTDBHelper.h"
 #include <time.h>
 #include <stdio.h>
 #include <string.h>
+#include "credentials.h"
 
-// Provide the token generation process info.
-#include "addons/TokenHelper.h"
-// Provide the RTDB payload printing info and other helper functions.
-#include "addons/RTDBHelper.h"
-
-// Insert your network credentials
-#define WIFI_SSID "KOSEI-THINKBOOK"
-#define WIFI_PASSWORD "password"
-
-// Insert Firebase project API Key
-#define API_KEY "AIzaSyBQztBx7W8Qed4b_85LvgAeaVYEeAdaBwA"
-
-// Insert RTDB URLefine the RTDB URL */
-#define DATABASE_URL "https://bdm-34-default-rtdb.firebaseio.com/"
-
-#define DEVICE_ID "kero"
-#define AD_PIN 33
-
-// Define Firebase Data object
-FirebaseData fbdo;
-
+// Firebase object
 FirebaseAuth auth;
 FirebaseConfig config;
+FirebaseData fbdo;
 
-unsigned long sendDataPrevMillis = 0;
-int count = 0;
-bool signupOK = false;
+unsigned long sendDataPrevT = 0;
+bool firebaseReadyFlag = false;
 
 // Function to get current timestamp in the required format
 void getCurrentTimestamp(char *buffer, size_t bufferSize)
@@ -46,7 +24,7 @@ void getCurrentTimestamp(char *buffer, size_t bufferSize)
   strftime(buffer, bufferSize, "%Y%m%d-%H%M%S", timeinfo);
 }
 
-// Function to add data to Firebase
+// Add data to Firebase
 void addData(float ampereValue, bool statusValue)
 {
   char timestamp[20];
@@ -61,7 +39,7 @@ void addData(float ampereValue, bool statusValue)
   // Add ampere value
   if (Firebase.RTDB.setFloat(&fbdo, amperePath, ampereValue))
   {
-    Serial.println("Ampere value added successfully");
+    Serial.printf("Ampere value added successfully: %f\n", ampereValue);
   }
   else
   {
@@ -72,7 +50,7 @@ void addData(float ampereValue, bool statusValue)
   // Add status value
   if (Firebase.RTDB.setBool(&fbdo, statusPath, statusValue))
   {
-    Serial.println("Status value added successfully");
+    Serial.printf("Status value added successfully: %d\n", statusValue);
   }
   else
   {
@@ -84,6 +62,12 @@ void addData(float ampereValue, bool statusValue)
 void setup()
 {
   Serial.begin(115200);
+  Serial.println("\n--------------------------------\n");
+  Serial.printf("Device: %s\n", DEVICE_ID);
+  Serial.printf("AD_PIN: %d\n", AD_PIN);
+  Serial.println("");
+
+  // Connect to Wi-Fi
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
   while (WiFi.status() != WL_CONNECTED)
@@ -91,50 +75,41 @@ void setup()
     Serial.print(".");
     delay(300);
   }
-  Serial.println();
-  Serial.print("Connected with IP: ");
+  Serial.print("\nConnected with IP: ");
   Serial.println(WiFi.localIP());
-  Serial.println();
+  Serial.println("");
 
-  /* Assign the api key (required) */
-  config.api_key = API_KEY;
-
-  /* Assign the RTDB URL (required) */
-  config.database_url = DATABASE_URL;
-
-  /* Sign up */
+  // Connect to Firebase
+  config.api_key = FIREBASE_API_KEY;
+  config.database_url = FIREBASE_DATABASE_URL;
   if (Firebase.signUp(&config, &auth, "", ""))
   {
-    Serial.println("ok");
-    signupOK = true;
+    Serial.println("Connected to Firebase\n");
+    firebaseReadyFlag = true;
   }
   else
   {
-    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+    Serial.printf("Failed to connect to Firebase: %s\n\n", config.signer.signupError.message.c_str());
   }
-
-  /* Assign the callback function for the long running token generation task */
-  config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
-
+  config.token_status_callback = tokenStatusCallback;
   Firebase.begin(&config, &auth);
   Firebase.reconnectWiFi(true);
 
   // Initialize time with JST (UTC+9)
   configTime(9 * 3600, 0, "pool.ntp.org", "time.nist.gov");
-
-  // Wait for time to be set
   while (!time(nullptr))
   {
     Serial.print(".");
     delay(1000);
   }
+  sendDataPrevT = millis();
 }
 
 void loop()
 {
-  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0))
+  if (Firebase.ready() && firebaseReadyFlag && millis() - sendDataPrevT > 15000)
   {
-    sendDataPrevMillis = millis();
+    sendDataPrevT = millis();
 
     uint16_t analog_adc = analogRead(AD_PIN);
     uint32_t analog_mv = analogReadMilliVolts(AD_PIN);
